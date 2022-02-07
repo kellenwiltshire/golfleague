22d8837ab345c11be548202e9878d2b79fdc8888
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-02-07 15:41:29 -0500

    working on connecting everything to the example db

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,20 +3,17 @@ import Image from 'next/image';
 
 export default function Hero(): JSX.Element {
 	return (
-		<main className='lg:relative min-h-screen'>
+		<main className='lg:relative '>
 			<div className='mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left'>
 				<div className='px-4 lg:w-1/2 sm:px-8 xl:pr-16'>
 					<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl'>
 						<span className='block xl:inline'>Golf League Tracker</span>
 					</h1>
 					<p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
-						Look out a ball is coming straight for you! Cheating on the next
-						hole Get a new Sand Iron Phil Mickelson Pull out your 3-wood and....
-						Condor Waggle your hips and swing Swing quickly, Start your
-						backswing, take it slow. Arnold Palmer While drinking beer
-						Completely missed the ball, oops! And your caddie gets cross. Splash
-						shot Pretend nothing just happened and smile. To distract your
-						opponent Get your 5 wood "Get in the hole!".
+						Look out a ball is coming straight for you! Cheating on the next hole Get a new Sand Iron Phil Mickelson
+						Pull out your 3-wood and.... Condor Waggle your hips and swing Swing quickly, Start your backswing, take it
+						slow. Arnold Palmer While drinking beer Completely missed the ball, oops! And your caddie gets cross. Splash
+						shot Pretend nothing just happened and smile. To distract your opponent Get your 5 wood "Get in the hole!".
 					</p>
 					<p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
 						Golf & Membership Group
@@ -33,14 +30,8 @@ export default function Hero(): JSX.Element {
 					</div>
 				</div>
 			</div>
-			<div className='relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-20 lg:right-0 lg:w-1/2 lg:h-96'>
-				<Image
-					className='object-cover'
-					src='/brand/coverphoto.jpg'
-					height={488}
-					width={330}
-					alt='Logo'
-				/>
+			<div className='relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full'>
+				<img className='w-full h-full object-cover' src='/coverphoto.jpg' alt='Logo' />
 			</div>
 		</main>
 	);
