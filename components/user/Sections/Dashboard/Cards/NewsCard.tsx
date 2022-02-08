133f06e7339eb8dda84f5674c9089813e007800b
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-02-08 16:30:37 -0500

    User Dashboard working

--- components/user/Sections/Dashboard/Cards/NewsCard.tsx
+++ components/user/Sections/Dashboard/Cards/NewsCard.tsx
@@ -28,15 +28,28 @@ export default function NewsCard(): JSX.Element {
 								Recent News
 							</h3>
 							<p className='mt-2 text-gray-500 text-xl'>{recentNews.title}</p>
-							<ReactMarkdown className='mt-2 text-sm text-gray-500'>
-								{shortBody}
-							</ReactMarkdown>
+							<ReactMarkdown className='mt-2 text-sm text-gray-500'>{shortBody}</ReactMarkdown>
 						</div>
 					</div>
 				</a>
 			</Link>
 		);
 	} else {
-		return <div></div>;
+		return (
+			<div className='sm:rounded-bl-lg relative group bg-white p-6 h-full'>
+				<div>
+					<span className='rounded-lg inline-flex p-3 ring-4 ring-white'>
+						<NewspaperIcon className='h-6 w-6' aria-hidden='true' />
+					</span>
+				</div>
+				<div className='mt-8'>
+					<h3 className='text-lg font-medium'>
+						<span className='inset-0' aria-hidden='true' />
+						Recent News
+					</h3>
+					<p className='mt-2 text-gray-500 text-xl'>No Recent News</p>
+				</div>
+			</div>
+		);
 	}
 }
--- components/user/Sections/Dashboard/Cards/SpecialCard.tsx
+++ components/user/Sections/Dashboard/Cards/SpecialCard.tsx
@@ -12,8 +12,7 @@ export default function SpecialCard(): JSX.Element {
 
 	if (recentSpecialFunction) {
 		const length = 100;
-		const shortBody =
-			recentSpecialFunction.details.substring(0, length) + '...';
+		const shortBody = recentSpecialFunction.details.substring(0, length) + '...';
 
 		return (
 			<Link href='/specialfunctions'>
@@ -29,18 +28,29 @@ export default function SpecialCard(): JSX.Element {
 								<span className='inset-0' aria-hidden='true' />
 								Special Functions
 							</h3>
-							<p className='mt-2 text-gray-500 text-xl'>
-								{recentSpecialFunction.name}
-							</p>
-							<ReactMarkdown className='mt-2 text-sm text-gray-500'>
-								{shortBody}
-							</ReactMarkdown>
+							<p className='mt-2 text-gray-500 text-xl'>{recentSpecialFunction.name}</p>
+							<ReactMarkdown className='mt-2 text-sm text-gray-500'>{shortBody}</ReactMarkdown>
 						</div>
 					</div>
 				</a>
 			</Link>
 		);
 	} else {
-		return <div></div>;
+		return (
+			<div className='sm:rounded-br-lg relative group bg-white p-6 h-full'>
+				<div>
+					<span className='rounded-lg inline-flex p-3 ring-4 ring-white'>
+						<NewspaperIcon className='h-6 w-6' aria-hidden='true' />
+					</span>
+				</div>
+				<div className='mt-8'>
+					<h3 className='text-lg font-medium'>
+						<span className='inset-0' aria-hidden='true' />
+						Special Functions
+					</h3>
+					<p className='mt-2 text-gray-500 text-xl'>No Upcoming Special Functions</p>
+				</div>
+			</div>
+		);
 	}
 }
