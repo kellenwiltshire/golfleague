db21d6d852fb446557b140928274227040c5c87c
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-21 13:27:56 -0400

    feat: All Scores Store added

---
+++ stores/AllScoresStore.tsx
@@ -0,0 +1,36 @@
+import { makeAutoObservable } from 'mobx';
+import { createContext, useContext, FC } from 'react';
+import { Score } from '@/utils/interfaces';
+
+class AllScoresStore {
+	allScores: Array<Score> = [];
+
+	constructor() {
+		makeAutoObservable(this);
+	}
+
+	updateScore(allScores: Array<Score>) {
+		this.allScores = allScores;
+	}
+}
+
+const AllScoresStoreContext = createContext<AllScoresStore>(
+	new AllScoresStore(),
+);
+
+const AllScoresStoreProvider: FC<{ store: AllScoresStore }> = ({
+	store,
+	children,
+}) => {
+	return (
+		<AllScoresStoreContext.Provider value={store}>
+			{children}
+		</AllScoresStoreContext.Provider>
+	);
+};
+
+const useAllScoresStore = () => {
+	return useContext(AllScoresStoreContext);
+};
+
+export { AllScoresStore, AllScoresStoreProvider, useAllScoresStore };
