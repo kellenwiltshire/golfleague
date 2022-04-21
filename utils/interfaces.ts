d161b8831c0cd2844b3df01e2242bd477d4d34f4
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-21 10:32:26 -0400

    feat: file created to hold all interfaces

---
+++ utils/interfaces.ts
@@ -0,0 +1,53 @@
+export interface Course {
+	additionalInfo: string;
+	address: string;
+	adminInfo: string;
+	contact: string;
+	created_at: string;
+	email: string;
+	id: number;
+	interval: number;
+	name: string;
+	phone: string;
+	pricing: string;
+	timeslots: number;
+	updated_at: string;
+}
+
+export interface Schedule {
+	course: Course;
+	created_at: string;
+	date: string;
+	game: string;
+	id: number;
+	start_time: string;
+	updated_at: string;
+}
+
+export interface User {
+	additionalInfo: string;
+	availability: [];
+	blocked: boolean;
+	carpool: string;
+	confirmed: boolean;
+	created_at: string;
+	email: string;
+	first_name: string;
+	id: number;
+	last_name: string;
+	initialLogin: false;
+	phone: string;
+	picture: string;
+	provider: string;
+	role: {
+		id: number;
+		name: string;
+		description: string;
+		type: string;
+	};
+	teeTime: boolean;
+	updated_at: string;
+	username: string;
+	weekendaway: boolean;
+	yearend: boolean;
+}
