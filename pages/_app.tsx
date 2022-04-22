c0ba57385728c23338602081abd3364af04b19f3
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-22 14:42:50 -0400

    feat: seperated out providers into component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,61 +4,24 @@ import nProgress from 'nprogress';
 import { Router } from 'next/router';
 import 'nprogress/nprogress.css';
 import { useState } from 'react';
+import Providers from '@/components/layout/Providers';
 
 Router.events.on('routeChangeStart', nProgress.start);
 Router.events.on('routeChangeError', nProgress.done);
 Router.events.on('routeChangeComplete', nProgress.done);
 
-import { UserStore, UserStoreProvider } from '@/stores/UserStore';
-import { ScheduleStore, ScheduleStoreProvider } from '@/stores/ScheduleStore';
-import { ScoreStore, ScoreStoreProvider } from '@/stores/ScoresStore';
-import { NewsStore, NewsStoreProvider } from '@/stores/NewsStore';
-import {
-	SpecialFunctionsStore,
-	SpecialFunctionsStoreProvider,
-} from '@/stores/SpecialFunctionsStore';
-import {
-	AllScoresStore,
-	AllScoresStoreProvider,
-} from '@/stores/AllScoresStore';
-import { AllUsersStore, AllUsersStoreProvider } from '@/stores/AllUsersStore';
-import { CoursesStore, CoursesStoreProvider } from '@/stores/CoursesStore';
-
-const userStore = new UserStore();
-const scheduleStore = new ScheduleStore();
-const scoreStore = new ScoreStore();
-const newsStore = new NewsStore();
-const specialFunctionsStore = new SpecialFunctionsStore();
-const allScoresStore = new AllScoresStore();
-const allUsersStore = new AllUsersStore();
-const coursesStore = new CoursesStore();
-
 function MyApp({ Component, pageProps }) {
 	const [signedIn, setSignedIn] = useState(false);
 	return (
-		<UserStoreProvider store={userStore}>
-			<ScheduleStoreProvider store={scheduleStore}>
-				<ScoreStoreProvider store={scoreStore}>
-					<NewsStoreProvider store={newsStore}>
-						<SpecialFunctionsStoreProvider store={specialFunctionsStore}>
-							<AllScoresStoreProvider store={allScoresStore}>
-								<AllUsersStoreProvider store={allUsersStore}>
-									<CoursesStoreProvider store={coursesStore}>
-										<Layout signedIn={signedIn} setSignedIn={setSignedIn}>
-											<Component
-												{...pageProps}
-												signedIn={signedIn}
-												setSignedIn={setSignedIn}
-											/>
-										</Layout>
-									</CoursesStoreProvider>
-								</AllUsersStoreProvider>
-							</AllScoresStoreProvider>
-						</SpecialFunctionsStoreProvider>
-					</NewsStoreProvider>
-				</ScoreStoreProvider>
-			</ScheduleStoreProvider>
-		</UserStoreProvider>
+		<Providers>
+			<Layout signedIn={signedIn} setSignedIn={setSignedIn}>
+				<Component
+					{...pageProps}
+					signedIn={signedIn}
+					setSignedIn={setSignedIn}
+				/>
+			</Layout>
+		</Providers>
 	);
 }
 
