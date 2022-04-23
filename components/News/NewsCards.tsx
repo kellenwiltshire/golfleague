c78f983aaafacf049e375178a3d00e1c7a21a6a4
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-04-22 20:18:20 -0400

    chore: refactor News Components

--- components/News/NewsCards.tsx
+++ components/News/NewsCards.tsx
@@ -13,11 +13,11 @@ export default function NewsCards({ post }): JSX.Element {
 		}
 	}, []);
 	return (
-		<div key={post.title} className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
-			<div className='w-full aspect-w-3 aspect-h-2 rounded-lg overflow-hidden flex justify-center'>
+		<div key={post.title} className='flex flex-col overflow-hidden rounded-lg shadow-lg'>
+			<div className='aspect-w-3 aspect-h-2 flex w-full justify-center overflow-hidden rounded-lg'>
 				<img src={image} className='h-44' />
 			</div>
-			<div className='flex-1 bg-white p-6 flex flex-col justify-between'>
+			<div className='flex flex-1 flex-col justify-between bg-white p-6'>
 				<div className='flex-1'>
 					<Link
 						href={{
@@ -25,9 +25,9 @@ export default function NewsCards({ post }): JSX.Element {
 							query: { slug: post.id },
 						}}
 					>
-						<a className='block mt-2'>
+						<a className='mt-2 block'>
 							<p className='text-xl font-semibold text-gray-900'>{post.title}</p>
-							<ReactMarkdown children={shortBody} className='mt-3 text-base text-gray-500' />
+							<ReactMarkdown className='mt-3 text-base text-gray-500'>{shortBody}</ReactMarkdown>
 						</a>
 					</Link>
 				</div>
