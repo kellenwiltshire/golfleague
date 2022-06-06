e81358fa15927de10737f0364923b7cace9d3bf2
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-06-06 18:56:15 -0400

    Feat: SSR Removed, SWR fully added

--- pages/user/[user].tsx
+++ pages/user/[user].tsx
@@ -5,10 +5,11 @@ import { CogIcon, HomeIcon, PencilIcon } from '@heroicons/react/outline';
 import Dashboard from '@/components/user/Sections/Dashboard';
 import Scores from '@/components/user/Sections/Scores';
 import Settings from '@/components/user/Sections/Settings';
-import { getUserData } from '@/utils/userFetch';
+import { fetchUser } from '@/utils/userFetch';
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
 import { useUserStore } from '@/stores/UserStore';
+import useSWR from 'swr';
 
 const navigation = [
 	{ num: 1, name: 'Dashboard', icon: HomeIcon },
@@ -16,68 +17,48 @@ const navigation = [
 	{ num: 3, name: 'Settings', icon: CogIcon },
 ];
 
-export default function User({ user }) {
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+export default function User() {
+	const { data: user, error: userError } = useSWR('/api/getUser', fetcher);
+
 	const userStore = useUserStore();
-
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		setLoading(false);
-
-		userStore.updateUser(user);
-	}, []);
+	userStore.updateUser(user);
 
 	const [openTab, setOpenTab] = useState(1);
 
-	if (loading) {
-		return <div>LOADING...</div>;
-	} else {
-		return (
-			<div className='flex w-full flex-row flex-wrap justify-center py-10'>
-				<div className='container'>
-					<UserHeader />
+	if (userError) return <div>Failed to load</div>;
+	if (!user) return <div>Loading...</div>;
+
+	return (
+		<div className='flex w-full flex-row flex-wrap justify-center py-10'>
+			<div className='container'>
+				<UserHeader />
+				{/* 3 column wrapper */}
+				<div className='mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8'>
 					{/* 3 column wrapper */}
-					<div className='mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8'>
-						{/* 3 column wrapper */}
-						<div className='py-6 pl-4 pr-6 sm:pl-6 lg:pl-8 xl:pl-0'>
-							<div className='flex items-center justify-between'>
-								<div className='flex-1 space-y-8'>
-									<div className='space-y-8 sm:flex sm:items-center sm:justify-between sm:space-y-0 xl:block xl:space-y-8'>
-										<Siderbar openTab={openTab} setOpenTab={setOpenTab} navigation={navigation} />
-									</div>
+					<div className='py-6 pl-4 pr-6 sm:pl-6 lg:pl-8 xl:pl-0'>
+						<div className='flex items-center justify-between'>
+							<div className='flex-1 space-y-8'>
+								<div className='space-y-8 sm:flex sm:items-center sm:justify-between sm:space-y-0 xl:block xl:space-y-8'>
+									<Siderbar openTab={openTab} setOpenTab={setOpenTab} navigation={navigation} />
 								</div>
 							</div>
 						</div>
-						<div className='bg-white lg:min-w-0 lg:flex-1'>
-							<div className={openTab === 1 ? 'block' : 'hidden'}>
-								<Dashboard />
-							</div>
-							<div className={openTab === 2 ? 'block' : 'hidden'}>
-								<Scores />
-							</div>
-							<div className={openTab === 3 ? 'block' : 'hidden'}>
-								<Settings />
-							</div>
+					</div>
+					<div className='bg-white lg:min-w-0 lg:flex-1'>
+						<div className={openTab === 1 ? 'block' : 'hidden'}>
+							<Dashboard />
+						</div>
+						<div className={openTab === 2 ? 'block' : 'hidden'}>
+							<Scores />
+						</div>
+						<div className={openTab === 3 ? 'block' : 'hidden'}>
+							<Settings />
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
-
-export const getServerSideProps: GetServerSideProps = async (props) => {
-	const cookies = parseCookies(props);
-	const jwt = cookies.jwt;
-	const userData = await getUserData(jwt);
-
-	return {
-		props: {
-			scores: userData.scores,
-			user: userData.user,
-			schedules: userData.schedules,
-			courses: userData.courses,
-			allScores: userData.allScores,
-		},
-	};
-};
